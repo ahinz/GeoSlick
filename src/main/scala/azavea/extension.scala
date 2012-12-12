0496265b46bfc07e5566db97954892f24cf04bcb
Adam Hinz <hinz.adam@gmail.com>
2012-12-12 11:27:37 -0500

    Cleanup a bit

--- src/main/scala/azavea/extension.scala
+++ src/main/scala/azavea/extension.scala
@@ -1,5 +1,7 @@
 package azavea.slick
 
+import language.implicitConversions
+
 import scala.slick.ast._
 import scala.slick.lifted._
 
@@ -16,11 +18,6 @@ final class GeometryColumnExtensionMethods[P1](val c: Column[P1]) extends AnyVal
   def distance[P2,R](e: Column[P2])(implicit om: o#arg[Geometry, P2]#to[Double, R]) =
     om(PostgisLibrary.Distance.column(n, Node(e)))
 }
-
-//type arg[B1, P1] = {
-//    type to[BR, PR] = OptionMapper2[B1, B1, BR, P1, P1, PR]
-// getOptionMapper2TT[B1, B2 : BaseTypeMapper, BR] = 
-//     OptionMapper2.plain.asInstanceOf[OptionMapper2[B1,B2,BR,B1,B2,BR]]
 
 trait PostgisConversions {
   implicit def geometryColumnExtensionMethods(c: Column[Geometry]) = new GeometryColumnExtensionMethods[Geometry](c)
--- src/main/scala/azavea/postgis.scala
+++ src/main/scala/azavea/postgis.scala
@@ -1,5 +1,7 @@
 package azavea.slick
 
+import language.implicitConversions
+
 import scala.collection.mutable.ArrayBuffer
 import scala.slick.ast._
 
@@ -17,9 +19,9 @@ import com.vividsolutions.jts.geom._
 import com.vividsolutions.jts.io.{WKBReader, WKBWriter, InputStreamInStream, WKTWriter}
 
 trait PostgisDriver extends PostgresDriver {  
-  case class SRID(srid: Int) extends ColumnOption[Nothing]
-  case class Dims(srid: Int) extends ColumnOption[Nothing]
-  case class GeoType[T <: Geometry](s: String) extends ColumnOption[Nothing]
+
+  //==================================================================
+  // Implicit support
 
   trait Implicits extends super.Implicits with PostgisConversions {
     implicit object GeomTypeMapper extends BaseTypeMapper[Geometry] {
@@ -37,29 +39,29 @@ trait PostgisDriver extends PostgresDriver {
   override val simple = new SimpleQL
 
   import simple._
-  
-  case class SelectOnlyApply(s: Node, fname: String) extends Library.SqlFunction(fname) {
-    override def toString = "SelectOnlyApply(%s)" format super.toString
 
-    override def typed[T : TypeMapper](ch: Node*): SelectOnlyApplyAux =
-      new SelectOnlyApplyAux(s, this, ch, implicitly[TypeMapper[T]])
-  }
+  //==================================================================
+  // Main mixin trait for Geo tables...
+  // should this be mixed into tables automatically if you're using
+  // the postgis driver?
 
-  class SelectOnlyApplyAux(val select: Node, s: Symbol, kids: Seq[Node], tp: Type) extends Apply(s, kids) with TypedNode {
-    def tpe = tp
-    override protected[this] def nodeRebuild(ch: IndexedSeq[scala.slick.ast.Node]) = Apply(sym, ch, tp)
-    override def nodeRebuildWithReferences(syms: IndexedSeq[Symbol]) = Apply(syms(0), children, tp)
-
-    override def toString = "SelectOnlyApplyAux(%s)" format super.toString
-  }
-  
-  object SelectOnlyApplyAux {
-    def unapply(s: SelectOnlyApplyAux):Option[Node] = Some(s.select)
-  }
+  case class SRID(srid: Int) extends ColumnOption[Nothing]
+  case class Dims(srid: Int) extends ColumnOption[Nothing]
+  case class GeoType[T <: Geometry](s: String) extends ColumnOption[Nothing]
 
   trait Geo { self: Table[_] =>
+    // Take a column and wrap it with a function that is called
+    // only on select. In this case, geometry fields get wrapped
+    // with ST_AsEWKB on select and are inserted as raw bytes
+    def wrapColumnWithSelectOnlyFunction[T](c: Column[Geometry], fname: String)(implicit ev: TypeMapper[T]) =
+      new Library.SqlFunction(fname) {
+        override def typed[T:TypeMapper](ch: Node*): SelectOnlyApply =
+          new SelectOnlyApply(c.nodeDelegate, this, ch, ev)
+      }.column(c.nodeDelegate)(ev)
+
     def wrapEWKB(c: Column[Geometry]):Column[Geometry] =
-      SelectOnlyApply(c.nodeDelegate, "ST_AsEWKB").column(c.nodeDelegate)
+      wrapColumnWithSelectOnlyFunction(c, "ST_AsEWKB")
+    //SelectOnlyApply(c.nodeDelegate, "ST_AsEWKB").column(c.nodeDelegate)
 
     def geoColumn[T <: Geometry](
       colName: String, srid: Int)(implicit geo: GeoType[T]):Column[Geometry] =
@@ -73,6 +75,9 @@ trait PostgisDriver extends PostgresDriver {
     implicit object MultiLineStringType extends GeoType[MultiLineString]("MULTILINESTRING")
     implicit object MultiPolygonType extends GeoType[MultiPolygon]("MULTIPOLYGON")
   }
+
+  //==================================================================
+  // Geometry type mapping
 
   override val typeMapperDelegates = new TypeMapperDelegates
 
@@ -106,16 +111,24 @@ trait PostgisDriver extends PostgresDriver {
     }
   }
 
+  //==================================================================
+  // We have to override the insert builder to correctly 
+  // handle our new SelectOnlyApply node type
+  // 
+  // Since the original version uses an inner recursive function
+  // we have to yank the entire thing out
+
   override def createInsertBuilder(node: Node): InsertBuilder =
     new InsertBuilder(node)
 
+  // all of this is from slick, except 'SelectOnlyApply'
   class InsertBuilder(node: Node) extends super.InsertBuilder(node) {
     override protected def buildParts(node: Node): PartsResult = {
       val cols = new ArrayBuffer[FieldSymbol]
       var table: String = null
       def f(c: Any): Unit = c match {
         case ProductNode(ch) => ch.foreach(f)
-        case SelectOnlyApplyAux(s) => f(s)
+        case SelectOnlyApply(s) => f(s)
         case t:TableNode => f(Node(t.nodeShaped_*.value))
         case Select(Ref(IntrinsicSymbol(t: TableNode)), field: FieldSymbol) =>
           if(table eq null) table = t.tableName
@@ -128,13 +141,28 @@ trait PostgisDriver extends PostgresDriver {
     }
   }
 
-  override def createColumnDDLBuilder(column: FieldSymbol, table: Table[_]): ColumnDDLBuilder = new ColumnDDLBuilder(column)
+  class SelectOnlyApply(val select: Node, s: Symbol, kids: Seq[Node], tp: Type) extends Apply(s, kids) with TypedNode {
+    def tpe = tp
+    override protected[this] def nodeRebuild(ch: IndexedSeq[scala.slick.ast.Node]) = Apply(sym, ch, tp)
+    override def nodeRebuildWithReferences(syms: IndexedSeq[Symbol]) = Apply(syms(0), children, tp)
 
-  override def createTableDDLBuilder(table: Table[_]): TableDDLBuilder = new TableDDLBuilder(table)
+    override def toString = "SelectOnlyApply(%s)" format super.toString
+  }
+  
+  object SelectOnlyApply {
+    def unapply(s: SelectOnlyApply):Option[Node] = Some(s.select)
+  }
+
+  //==================================================================
+  // Building geometry columns is such fun
+
+  override def createColumnDDLBuilder(column: FieldSymbol, table: Table[_]): ColumnDDLBuilder = new ColumnDDLBuilder(column)
 
   class ColumnDDLBuilder(column: FieldSymbol) extends super.ColumnDDLBuilder(column) {
     def isGeometryColumn = sqlType == "geometry"
 
+    /** Seems to be issues when setting
+        these explicitly to None (init order?) */
     var srid: Option[Int] = _
     var dims: Option[Int] = _
     var geoType: Option[String] = _
@@ -149,7 +177,7 @@ trait PostgisDriver extends PostgresDriver {
         case GeoType(s) => geoType = Some(s)
         case _ => super.handleColumnOption(o)
       }
-   
+
     def getAddGeometry(tbl: String):Option[String] = {
       if (isGeometryColumn) {
         val sb = new StringBuilder
@@ -157,7 +185,6 @@ trait PostgisDriver extends PostgresDriver {
         val geomType = geoType.getOrElse(sys.error("Missing required geo type"))
         val d = dims.getOrElse(2)
         sb append "SELECT AddGeometryColumn("
-        //sb append quoteIdentifier("public") append ","
         sb append quoteString(tbl) append ","
         sb append quoteString(column.name) append ","
         sb append s append ","
@@ -168,7 +195,8 @@ trait PostgisDriver extends PostgresDriver {
         None
       }
     }
-
+    
+    // this must already exist....?
     def quoteString(s: String) = "'%s'" format s
 
     override def appendColumn(sb: StringBuilder) {
@@ -184,19 +212,20 @@ trait PostgisDriver extends PostgresDriver {
     }
   }
 
-  class TableDDLBuilder(table: Table[_]) extends super.TableDDLBuilder(table) {
-    override def buildDDL: DDL = {
-      if(primaryKeys.size > 1)
-        throw new SlickException("Table "+table.tableName+" defines multiple primary keys ("
-          + primaryKeys.map(_.name).mkString(", ") + ")")
-      new DDL {
-        val createPhase1 = Iterable(createTable) ++ primaryKeys.map(createPrimaryKey) ++ indexes.map(createIndex) ++ createGeomColumns
-        val createPhase2 = foreignKeys.map(createForeignKey)
-        val dropPhase1 = foreignKeys.map(dropForeignKey)
-        val dropPhase2 = primaryKeys.map(dropPrimaryKey) ++ Iterable(dropTable)
-      }
-    }
+  //==================================================================
+  // building geometry tables
+  override def createTableDDLBuilder(table: Table[_]): TableDDLBuilder = new TableDDLBuilder(table)
 
+  class TableDDLBuilder(table: Table[_]) extends super.TableDDLBuilder(table) {
+    override def buildDDL: DDL =
+      super.buildDDL ++ new DDL {
+        val createPhase1 = createGeomColumns
+        val createPhase2 = Seq()
+        val dropPhase1 = Seq()
+        val dropPhase2 = Seq()
+      }
+
+    //TODO: this method should be refactored
     override protected def createTable: String = {
       val b = new StringBuilder append "create table " append quoteIdentifier(table.tableName) append " ("
       for(n <- table.create_*) {
