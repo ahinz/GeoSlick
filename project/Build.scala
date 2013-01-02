f82c2da18d42423191ff91142bf2568314ec5f96
Adam Hinz <hinz.adam@gmail.com>
2013-01-02 13:49:23 -0500

    Move up to postgres 9.1

--- project/Build.scala
+++ project/Build.scala
@@ -17,7 +17,7 @@ object MyBuild extends Build {
         "com.typesafe" % "slick_2.10.0-RC5" % "0.11.2",
         "org.slf4j" % "slf4j-nop" % "1.6.4",
         //"postgresql" % "postgresql" % "8.4-702.jdbc4",
-        "postgresql" % "postgresql" % "9.1-903.jdbc4",
+        "postgresql" % "postgresql" % "9.1-901.jdbc4",
         "com.vividsolutions" % "jts" % "1.12",
         "org.scalatest" % "scalatest_2.10.0" % "2.0.M5" % "test"
     ),
